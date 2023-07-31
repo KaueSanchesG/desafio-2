27bdbf71a9243df8045b81809bc1576ed72be248
KaueSanchesG <kauesanches123@gmail.com>
2023-07-31 10:58:23 -0300

    Criação de novas funções para o projeto

--- src/main/java/org/main/Func.java
+++ src/main/java/org/main/Func.java
@@ -1,47 +1,77 @@
 package org.main;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Func {
     static Scanner sc = new Scanner(System.in);
-    public static int menu(){
+
+    public static int menu(List<Cliente> clienteList) {
         int opc;
-        System.out.println("Bem vindo ao sistema de vendas, digite uma das opções abaixo:");
+        System.out.println("Bem-vindo ao sistema de vendas, digite uma das opções abaixo:");
         System.out.print("1 - Realizar pedido\n2 - Cliente\n3 - Sair\n");
         opc = sc.nextInt();
-        if (opc == 1){
+        if (opc == 1) {
             System.out.println("Aqui realiza pedidos");
-        }else {
-            menuCliente(new ArrayList<>());
+        }
+        if (opc == 2) {
+            menuCliente(clienteList);
         }
         return opc;
     }
-//    List<Cliente> cliente
-    public static int menuCliente(List<Cliente> clienteList){
+
+    public static List<Cliente> menuCliente(List<Cliente> clienteList) {
         int opc;
         System.out.println("<---------- Cliente ---------->");
-        System.out.print("1 - Procurar cliente \n2 - Editar cliente\n");
+        System.out.print("1 - Cadastrar \n2 - Procurar \n3 - Editar\n");
         opc = sc.nextInt();
-        if (opc == 1){
-            String nome;
-            System.out.println("Digite o nome do cliente a procurar:");
-            nome=sc.next();
-            for (int i=0; i<clienteList.size(); i++)
-            if(clienteList.get(i).getNome().equals(nome)){
-                        System.out.println("Nome: " + clienteList.get(i).getNome());
-
-                        List<Endereco> enderecos = clienteList.get(i).getEndereco();
-                        System.out.println("Endereços:");
-                        for (Endereco endereco : enderecos) {
-                            System.out.println("- Rua: " + endereco.getRua() + ", Número: " + endereco.getNumero() + "\n");
-                        }
+        switch (opc) {
+            case 1 -> {
+                System.out.println("Quantos clientes vai adicionar?");
+                int i = sc.nextInt();
+                for (int contaCliente = 0; contaCliente < i; contaCliente++) {
+                    System.out.println("Digite o nome do " + (contaCliente + 1) + "° cliente:");
+                    String nome = sc.next();
+                    System.out.println("Quantos endereços o " + (contaCliente + 1) + "° cliente possui?");
+                    int j = sc.nextInt();
+                    List<Endereco> enderecoList = new ArrayList<>();
+                    for (int contaEnd = 0; contaEnd < j; contaEnd++) {
+                        System.out.println("Endereço " + (j + 1));
+                        System.out.println("Digite a rua:");
+                        String rua = sc.next();
+                        System.out.println("Agora, o numero da casa:");
+                        int numero = sc.nextInt();
+                        enderecoList.add(new Endereco(rua, numero));
+                    }
+                    clienteList.add(new Cliente(nome, enderecoList));
+                }
             }
-        }else {
-            System.out.println("Aqui edita cliente");
+            case 2 -> {
+                if (clienteList.size() != 0) {
+                    String nome;
+                    System.out.println("Digite o nome do cliente a procurar:");
+                    nome = sc.next();
+                    for (int i = 0; i < clienteList.size(); i++) {
+                        if (clienteList.get(i).getNome().equals(nome)) {
+                            System.out.println("Nome: " + clienteList.get(i).getNome());
+                            List<Endereco> enderecos = clienteList.get(i).getEndereco();
+                            System.out.println("Endereços:");
+                            for (Endereco endereco : enderecos) {
+                                System.out.println("- Rua: " + endereco.getRua() + ", Número: " + endereco.getNumero() + "\n");
+                            }
+                        } else {
+                            System.out.println("Esse cliente não existe...");
+                        }
+                    }
+                } else {
+                    System.out.println("Cadastre um cliente antes para poder procurar...");
+                }
+            }
+            case 3 -> {
+                System.out.println("Aqui edita cliente");
+            }
         }
-        return 1;
+        return clienteList;
     }
 }
--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -1,10 +1,14 @@
 package org.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Cliente> clientes = new ArrayList<>();
         int opc;
-        do{
-            opc = Func.menu();
-        }while (opc<3);
+        do {
+            opc = Func.menu(clientes);
+        } while (opc < 3);
     }
 }
